87f693d048a4e04cfdf1797bbfc9a30b0d6ed9ce
Vladyslav Zhukovskyi <vzhukovsky@e2r10p3.unit.ua>
2019-11-21 20:07:30 +0200

    split

--- Pathfinder/Pathfinder.c
+++ Pathfinder/Pathfinder.c
@@ -2,21 +2,34 @@
 #include "stdio.h"
 
 int main(int ac, char **av) {
-    char *file = NULL;
-    //char *islands = NULL;
-	int PointIslands = 0;
-
+    char *file = NULL; // file contain
+    char **islands; 
+	int PointIslands = 0; // apex
+    char **range;
+    int k = 0;
 
     file = mx_file_to_str(av[1]);
     PointIslands = mx_atoi(&file[0]);
-    //islands = *mx_strsplit(file, '\n');
-
+    char **islands = mx_strsplit(file, '\n');
     if (ac > 0) {
         
-        printf("point = %d", PointIslands);
+        range = mx_strnew(mx_strlen(mx_isdigit(*islands)));
+        for (int i = 1; *islands[i]; i++) {
+            for (int j = 0; *islands[j]; j++) {
+                if (mx_isdigit(*islands))
+                    range[i][k] = *islands[j++];
+            }
+            printf("%s\n", range);
+        }
+            
+     
 
+        //printf("point = %d", PointIslands);
+    // error case
     // if (ac > 2)
     //     mx_printerr("usage: ./pathfinder [filename]");
 
     }
 }
+
+
